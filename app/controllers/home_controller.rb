2d98ba4917235e5e9f6a50dba86a11853e495594
mehulambule123@gmail.com <mehulambule123@gmail.com>
2023-12-05 00:41:25 +0530

    Formatted and added comments for them

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
 
     def index
         if signed_in?
+            # If signed in, redirect to the user's tasks page with 'done' parameter set to false
             redirect_to user_task_path(done: false)
         end
+        # If not signed in, fetch public tasks
         @tasks = Task.where(public: true)
     end
     
--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,41 +1,48 @@
 class TaskController < ApplicationController
+  before_action :authenticate_user!
+
     def index
         @tasks = Task.where(user: current_user,done: params[:done])
     end
 
+    # GET /tasks/new
+    # Displays a form for creating a new task
     def create
-        @task = current_user.tasks.new(task_params)
-        
-        if @task.save
-          redirect_to user_task_path(done: false), notice: 'Task was successfully created.'
-        else
-          render :new
+      @task = current_user.tasks.new(task_params)
+      if @task.save
+        redirect_to user_task_path(done: false), notice: 'Task was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    # PATCH/PUT /tasks/:id
+    # Updates the status of a task for done
+    def update
+      @task = Task.find(params[:id])
+  
+      if @task.update(done: true)
+        respond_to do |format|
+          format.html { redirect_to user_task_path(done: true), notice: 'Task was successfully updated.' }
+          format.js   # This responds to AJAX requests (if you're using remote: true in the form)
+        end
+      else
+        respond_to do |format|
+          format.html { render :index } # Render the index view in case of errors
+          format.js   # This responds to AJAX requests (if you're using remote: true in the form)
         end
       end
+    end  
 
-      def update
-        @task = Task.find(params[:id])
-    
-        if @task.update(done: true)
-          respond_to do |format|
-            format.html { redirect_to user_task_path(done: true), notice: 'Task was successfully updated.' }
-            format.js   # This responds to AJAX requests (if you're using remote: true in the form)
-          end
-        else
-          respond_to do |format|
-            format.html { render :index } # Render the index view in case of errors
-            format.js   # This responds to AJAX requests (if you're using remote: true in the form)
-          end
-        end
-      end
+    # GET /tasks/new
+    # Displays a form for creating a new task
+    def new
+      @task = Task.new
+    end
 
-      def new
-        @task = Task.new
-      end
-
-      private
-    
-      def task_params
-        params.require(:task).permit(:title, :description, :done, :public)
-      end
+    private
+  
+    def task_params
+      params.require(:task).permit(:title, :description, :done, :public)
+    end
 end
