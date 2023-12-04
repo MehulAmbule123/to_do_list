3283654a7905352ec6777b0491c45dcc9617bebb
mehulambule123@gmail.com <mehulambule123@gmail.com>
2023-12-04 21:35:27 +0530

    Generated custom Devise controllers for user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
+  get "/user/task" ,to: "task#index", as: "user_task"
   # Defines the root path route ("/")
   # root "articles#index"
 end
